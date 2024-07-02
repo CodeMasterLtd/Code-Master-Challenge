349a55b3094b40c727bf86af7c3133854896ff6a
CodeMasterLtd <info@codemaster.ltd>
2024-07-02 11:44:20 +0100

    V2.0.0.0

---
+++ config.js
@@ -0,0 +1,19 @@
+// 2024 | Code Master!
+export const Config = {
+  // DISCOUNTS
+  TwentyOFF: '20%/CM2024/CMC', // 20% CODE
+  TwentyOFFMsg: '20% OFF ALL PRODUCTS', // 20% OFF ALL PRODUCTS
+
+  TenOFF: '10%/CM2024/CMC', // 10% CODE
+  TenOFFMsg: '10% OFF ALL PRODUCTS', // 10% OFF ALL PRODUCTS
+
+  FiveOFF: '5%/CM2024/CMC', // 5% CODE
+  FiveOFFMsg: '5% OFF ALL PRODUCTS', // 5% OFF ALL PRODUCTS
+
+  FREE: 'FREE/CM2024/CMC', // FREE CODE
+  FREEMsg: 'FREE ON ALL PRODUCTS', // FREE ON ALL PRODUCTS
+
+  Timer: 30, // Timer in seconds
+
+};
+// 2024 | Code Master!
